5a4a3eea3c2db9524ff6f3714918499b92d03961
Faizan59khan <khan_faizan59@yahoo.com>
2023-07-04 11:22:07 +0500

    rm .env from tracking

--- JWT-Basics/src/app.ts
+++ JWT-Basics/src/app.ts
@@ -15,45 +15,25 @@ connectDB();
 
 app.use("/", userRoutes);
 
-app.post("/login", (req: Request, res: Response) => {
-  try {
-    const { username, password } = req.body;
-
-    // Validate user credentials (e.g., compare with data in the database)
-
-    const token = jwt.sign(
-      { username },
-      process.env.JWT_SECRET_KEY || "faizan"
-    );
-    res.status(200).json({ token });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
 app.get("/protected", (req: Request, res: Response) => {
-  try {
-    // Extract the token from the Authorization header
-    const token = req.headers.authorization;
+	try {
+		// Extract the token from the Authorization header
+		const token = req.headers.authorization;
 
-    // Verify and decode the token
-    if (token) {
-      const decodedToken = jwt.verify(
-        token,
-        process.env.JWT_SECRET_KEY || "faizan"
-      );
-    }
+		// Verify and decode the token
+		if (token) {
+			const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY || "faizan");
+		}
 
-    // Access the authenticated user's data (e.g., fetch from the database)
-    // Replace this code with your own logic
+		// Access the authenticated user's data (e.g., fetch from the database)
+		// Replace this code with your own logic
 
-    res.status(200).json({ message: "Protected route accessed successfully" });
-  } catch (error) {
-    res.status(401).json({ error: "Unauthorized" });
-  }
+		res.status(200).json({ message: "Protected route accessed successfully" });
+	} catch (error) {
+		res.status(401).json({ error: "Unauthorized" });
+	}
 });
 
 app.listen(3000, () => {
-  console.log("server connected at port 3000");
+	console.log("server connected at port 3000");
 });
--- JWT-Basics/src/routes/userRoutes.ts
+++ JWT-Basics/src/routes/userRoutes.ts
@@ -1,8 +1,16 @@
 import express, { Router } from "express";
-import { createNewUser, getAllUsers } from "../controllers/userControllers";
+import {
+	createNewUser,
+	deleteUserById,
+	getAllUsers,
+	userLogin,
+} from "../controllers/userControllers";
 
 const router = Router();
 
+router.get("/users", getAllUsers);
 router.post("/register", createNewUser);
+router.post("/login", userLogin);
+router.delete("/users/:id", deleteUserById);
 
 export default router;
